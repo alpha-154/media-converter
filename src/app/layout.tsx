4c53c2c89df5749c70ff463aee0d38cc083ccb24
alpha-154 <mrhrakib07@gmail.com>
2024-09-26 14:09:51 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
+
+//API_ENVIRONMENT_VARIABLE=CLOUDINARY_URL=CLOUDINARY_URL=cloudinary://342191488488758:t5UlGgSte6leIw2pUizhzDokkOc@dyy7hjubd
